0cbd0df701112eda815f22f3d934d3160844cb08
peterkiman05 <P.CTHOLE5@GMAIL.COM>
2024-11-23 22:02:13 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+name=input("Hey, what is your name: ")
+print("Hello "+name +" my love")
+print("by the way,"+ name +" it's been a long day without a text from you ")
+question=input("Is everything ok?: ")
+if question=="yes":
+    print("glad to hear that " +name+" my love")
+else:
+    print("what a dissapointment "+name +" thought you loved me: ")
+love=int(input("if u still do how much: "))
+if love <80:
+    print("go fuck yourself")
+elif love>80:
+    print("I love u more sweetheart")
+
+
+
+
+
+
